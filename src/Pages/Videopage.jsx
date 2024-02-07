837dc9b773ce4b8d707fe5e6049561fa45cadc43
mukilan2815 <mukilan2808@gmail.com>
2024-02-07 22:53:59 +0530

    2nd

--- src/Pages/Videopage.jsx
+++ src/Pages/Videopage.jsx
@@ -1,34 +1,52 @@
-import React, { useRef } from "react";
-import ReactPlayer from "react-player";
+import React from "react";
 
 const VideoPage = () => {
-  const playerRef = useRef(null);
+  // Sample video data
+  const sampleVideo = {
+    id: 1,
+    description: "Sample Video",
+    videoUrl: "abc123", // Replace with the actual YouTube video ID
+  };
+
+  const openVideoInNewTab = (url) => {
+    url = url.split("/").pop();
+    window.open(`https://www.youtube.com/embed/${url}`, "_blank");
+    console.log(`https://www.youtube.com/embed/${url}`);
+  };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <ReactPlayer
-        ref={playerRef}
-        url={`https://www.youtube.com/watch?v=e7a4amOfEfU`}
-        width="90%"
-        height="90%"
-        controls={false}
-        volume={1}
-        playbackRate={1}
-        muted={false}
-        loop={false}
-        config={{
-          youtube: {
-            playerVars: {
-              modestbranding: 1,
-              disablekb: 1,
-              controls: 0,
-              showinfo: 0,
-              rel: 0,
-              origin: window.location.origin,
-            },
-          },
-        }}
-      />
+    <div className="bg-teal-500 min-h-screen p-8">
+      <h2 className="text-4xl font-bold text-white text-center mb-8">
+        Student Dashboard
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        <div
+          key={sampleVideo.id}
+          className="bg-white rounded-lg overflow-hidden shadow-md"
+        >
+          <iframe
+            width="100%"
+            height="200"
+            src={`https://www.youtube.com/embed/${sampleVideo.videoUrl}`}
+            title={`Embedded YouTube Video ${sampleVideo.id}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="rounded-t-md"
+          ></iframe>
+          <div className="p-4">
+            <p className="text-gray-800 font-semibold mb-2">
+              {sampleVideo.description}
+            </p>
+            <button
+              className="bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-teal-600 focus:outline-none"
+              onClick={() => openVideoInNewTab(sampleVideo.videoUrl)}
+            >
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
